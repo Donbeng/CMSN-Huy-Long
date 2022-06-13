a323f80c19ba08729db2712cfb4ff8a86b5769d4
Donbeng <107378355+Donbeng@users.noreply.github.com>
2022-06-13 09:29:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,9 @@ const CONFIG = {
     desc: 'Iu em hongg?',
     btnYes: 'Hong iu 😥',
     btnNo: 'Iu em 😘',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tuổi mới phải thật hạnh phúc nhé',
     btnAccept: 'Hứaaaaa',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/lnhianh' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
